2ec34e337e1ab6aacc5f992ee1dfeaf76f7cafae
Lydia Wallace <lydia.wallace@outlook.com>
2022-04-30 19:40:03 +0100

    bug solved

--- src/app.js
+++ src/app.js
@@ -9,16 +9,3 @@ app.use(express.json());
 app.use('/artist', artistRouter);
 
 module.exports = app;
-
-// const express = require('express');
-// const artistRouter = require('./routes/artist');
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.use('/artist', artistRouter);
-
-// app.get('/', (req, res) => { res.status(200).send('hello world')});
-
-// module.exports = app;
--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,21 +1,20 @@
 // src/controllers/artist.js
-exports.create = (req, res) => {
-  res.sendStatus(201);
-}
+const getDb = require('../services/db');
 
-// const getDb = require('../services/db.js');
+exports.create = async (req, res) => {
+  const db = await getDb();
+  const { name, genre } = req.body;
 
-// exports.create = async (req, res) => {
-//   const db = await getDb();
-//   const { name, genre } = req.body;
+  try {
+    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+      name,
+      genre,
+    ]);
 
-//   try {
-//     await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500).json(err);
+  }
 
-//     res.sendStatus(201);
-//   } catch (err) {
-//     res.sendStatus(500).json(err);
-//   }
-
-//   db.close();
-// };
+  db.close();
+};
