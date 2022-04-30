1903a5cfb1670bf025e68c2b063bed46a9a05d3f
Lydia Wallace <lydia.wallace@outlook.com>
2022-04-30 11:38:06 +0100

    tests passing at this point; our first test

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// src/app.js
 const express = require('express');
 const artistRouter = require('./routes/artist');
 
@@ -8,3 +9,16 @@ app.use(express.json());
 app.use('/artist', artistRouter);
 
 module.exports = app;
+
+// const express = require('express');
+// const artistRouter = require('./routes/artist');
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.use('/artist', artistRouter);
+
+// app.get('/', (req, res) => { res.status(200).send('hello world')});
+
+// module.exports = app;
