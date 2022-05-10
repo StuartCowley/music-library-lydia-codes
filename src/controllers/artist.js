5a0fffbbf6d6f8884da1bc1e205f7edec2c11fb2
Lydia Wallace <lydia.wallace@outlook.com>
2022-05-10 20:27:23 +0100

    delete artist challenge completed.

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,4 +1,5 @@
 // src/controllers/artist.js
+// const db = require('../services/db');
 const getDb = require('../services/db');
 
 exports.create = async (req, res) => {
@@ -32,14 +33,49 @@ exports.read = async (_, res) => {
   db.close();
 };
 
-exports.readById = async (request, result) => {
+exports.readById = async (request, response) => {
   const db = await getDb();
   const { artistId } = request.params;
+
   const [[artist]] = await db.query('SELECT * FROM Artist WHERE id = ?', [artistId]);
 
-  if (!artist) { result.sendStatus(404); } else {
-    result.status(200).json(artist);
+  if (!artist) { response.sendStatus(404); } else {
+    response.status(200).json(artist);
   }
 
   db.close();
+};
+
+exports.update = async (request, response) => {
+  const databaseConnection = await getDb();
+  const { artistId } = request.params;
+  const dataToInsert = request.body;
+  try {
+    const [{ affectedRows: partOfResultObjectThatSaysWhatIsAffected }] = await databaseConnection.query('UPDATE Artist SET ? WHERE id = ?', [dataToInsert, artistId]);
+
+    if (!partOfResultObjectThatSaysWhatIsAffected) {
+      response.sendStatus(404);
+    } else {
+      response.status(200).send(`Artist ${artistId} has been updated`);
+    }
+  } catch (err) {
+    response.sendStatus(500);
+  }
+
+  databaseConnection.close();
+};
+
+exports.delete = async (request, response) => {
+  const databaseConnection = await getDb();
+  const { artistId } = request.params;
+  try {
+    const [{ affectedRows: partOfResultObjectThatSaysWhatIsAffected }] = await databaseConnection.query('DELETE FROM Artist WHERE id = ?', [artistId]);
+    if (!partOfResultObjectThatSaysWhatIsAffected) { response.sendStatus(404); } else {
+      response.status(200).json(`${artistId} has been deleted`);
+    }
+    response.status(200).json(`${artistId} has been deleted`);
+  } catch (err) {
+    response.sendStatus(500);
+  }
+  databaseConnection.close();
 };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/', artistController.create);
 router.get('/', artistController.read);
 router.get('/:artistId', artistController.readById);
+router.patch('/:artistId', artistController.update);
+router.delete('/:artistId', artistController.delete);
 
 module.exports = router;
